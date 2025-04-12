88c436f18eec1af0c6a4668d308c8e7c6cd9998c
Argha-coding <ghoshargha777@gmail.com>
2025-04-12 09:42:39 +0000

    data types

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,9 @@
-console.log("argha");
+const accountId =11221
+let accountEmail = "argha@gmail.com"
+var accountPassword = "265332"
+accountCity = "jaipur"
+accountEmail = "ARGHA334@gmail.com"
+accountPassword = "232434"
+accountCity = "kolkata"
+console.table([accountId,accountEmail, accountPassword,accountCity])
+/*prefer not to use for scope problem */ 
