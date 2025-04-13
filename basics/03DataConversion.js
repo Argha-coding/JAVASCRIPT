050312c39c9c370e22ab016cffe197c78e986c4a
Argha-coding <ghoshargha777@gmail.com>
2025-04-13 16:04:40 +0000

    DataConversion

---
+++ basics/03DataConversion.js
@@ -0,0 +1,14 @@
+let score = 33
+let Isloggedin = Number(score);
+console.log(Isloggedin)
+console.log(typeof(Isloggedin))
+
+let value =  "55"
+console.log(typeof(value));
+let value1 =  "55abc"
+console.log(typeof(value1));
+console.log(value1);
+let Score1 = "44abc"
+let ValueInNumber = Number(Score1)
+console.log(ValueInNumber);
+console.log(typeof(ValueInNumber));
